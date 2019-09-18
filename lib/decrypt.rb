fa77a24acae6cb6bbfbf31fd3d9a969b0c6aa012
Zac Isaacson <isaacson.zm@gmail.com>
2019-09-17 18:32:43 -0600

    Add Decrypt class and test

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,7 +1,24 @@
+require './module/encryptable'
+
 class Decrypt
+  include Encryptable
 
-
-
+  def decrypt_message(message, key, date)
+    characters = ("a".."z").to_a << " "
+    shift = number_shift(key, date)
+    split_message = message_breakdown(message)
+    new_message = []
+    split_message.map do |letter|
+      if characters.include?(letter)
+        new_character = characters.rotate(characters.index(letter) - shift[0])
+        new_message << new_character[0]
+        shift.rotate!
+      else
+        new_message << letter
+      end
+    end
+    new_message.join
+  end
 
 
 end
--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -3,7 +3,22 @@ require './lib/decrypt'
 
 class DecryptTest < Minitest::Test
 
+  def setup
+    @decrypt = Decrypt.new
+  end
 
+  def test_it_exists
+    assert_instance_of Decrypt, @decrypt
+  end
 
+  def test_decrypt_message
+
+    assert_equal "hello", @decrypt.decrypt_message("keder", "02715", "040895")
+  end
+
+  def test_decrypt_message_characters
+
+    assert_equal "hello!", @decrypt.decrypt_message("keder!", "02715", "040895")
+  end
 
 end
